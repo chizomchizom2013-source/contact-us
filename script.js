dd33e5da07466e73cff52951c727f7011451a8ae
chisom <chizomchizom2013@gmail.com>
2026-08-18 11:09:09 +0100

    first commit

--- script.js
+++ script.js
@@ -1,78 +1,65 @@
 function agecheck(number) {
-    if (number >= 18) {
-        console.log("you are an Adult");
-        
-    } else {
-        console.log("you are a minor");
-        
-    }
+  if (number >= 18) {
+    console.log("you are an Adult");
+  } else {
+    console.log("you are a minor");
+  }
 }
 
-agecheck(19)
+agecheck(19);
 
 function numbercheck(nc) {
-    if (nc > 0) {
-        console.log("positive");
-    }
-    else if (nc < 0) {
-        console.log("negative");
-    }
-    else{
-        console.log("Zero");
-        
-    }
+  if (nc > 0) {
+    console.log("positive");
+  } else if (nc < 0) {
+    console.log("negative");
+  } else {
+    console.log("Zero");
+  }
 }
 
-numbercheck(0)
+numbercheck(0);
 
-
-let username="chisom"
-let pasword="ch123"
-function login(us , pa) {
-    if (us === username || pasword === pa) {
-        console.log("login successful");
-        
-    } else {
-        console.log("invalid credentials");
-        
-    }
+let username = "chisom";
+let pasword = "ch123";
+function login(us, pa) {
+  if (us === username || pasword === pa) {
+    console.log("login successful");
+  } else {
+    console.log("invalid credentials");
+  }
 }
 
-login("chisom ", "ch123")
-
+login("chisom ", "ch123");
 
 const checknumber = (num) => {
-    if (num/2 === 0) {
-        console.log("even number");
-    } else {
-        console.log("odd number");
-    }
-}
+  if (num % 2 === 0) {
+    console.log("even number");
+  } else {
+    console.log("odd number");
+  }
+};
 
-checknumber(1)
+checknumber(1);
 
+let gna = 80;
+let gnb = 79;
+let gnc = 69;
+let gnd = 59;
+let gnf = 49;
 
-let gna=80
-let gnb=79
-let gnc=69
-let gnd=59
-let gnf=49
-
-const gradecheck=(grade)=>{
-    if (grade>= gna || grade>= 100) {
-        return("A");
-        
-    }else if (grade<=gnb && grade>= 70 ) {
-        return("B")
-    } else if(grade<=gnc && grade>= 60){
-        return("C")
-    }else if(grade<=gnd &&  grade>= 50){
-        return("D")
-    }else if (grade<=gnf && grade>=0) {
-        return("F")
-    }
-}
+const gradecheck = (grade) => {
+  if (grade >= gna || grade >= 100) {
+    return "A";
+  } else if (grade <= gnb && grade >= 70) {
+    return "B";
+  } else if (grade <= gnc && grade >= 60) {
+    return "C";
+  } else if (grade <= gnd && grade >= 50) {
+    return "D";
+  } else if (grade <= gnf && grade >= 0) {
+    return "F";
+  }
+};
 
 console.log(gradecheck(100));
-
-
